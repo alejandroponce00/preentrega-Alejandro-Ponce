6f1d35de4d6d70f5dd6543f2e6038f2116e3b490
alejandro <ts.com>
2025-10-25 15:37:12 -0300

    acomodando el layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,9 +3,9 @@ import Header from './Header';
 
 function Layout({ children, cartItemsCount, search, setSearch, user, logout }) {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header cartItemsCount={cartItemsCount} search={search} setSearch={setSearch} user={user} logout={logout} />
-      <main className="max-w-7xl mx-auto px-4 py-8">
+      <main className="flex-1 w-full flex flex-col items-center justify-center px-4 sm:px-6 md:px-10 py-4 sm:py-6 md:py-8 xl:px-0 xl:max-w-full" style={{maxWidth: '100vw'}}>
         {children}
       </main>
     </div>
